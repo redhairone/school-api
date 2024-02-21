f034359bb72133fd19ccf630f6b4c6a3a159fcf3
redhairone <dominik.ciesielski1997@gmail.com>
2024-02-21 18:23:42 +0100

    Added error codes to the exceptions.

--- src/main/java/com/ciesielski/schoolapi/domain/exceptions/NoSuchParentException.java
+++ src/main/java/com/ciesielski/schoolapi/domain/exceptions/NoSuchParentException.java
@@ -1,7 +1,11 @@
 package com.ciesielski.schoolapi.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.NoSuchElementException;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NoSuchParentException extends NoSuchElementException {
 
     public NoSuchParentException(final long id) {
--- src/main/java/com/ciesielski/schoolapi/domain/exceptions/NoSuchSchoolException.java
+++ src/main/java/com/ciesielski/schoolapi/domain/exceptions/NoSuchSchoolException.java
@@ -1,9 +1,12 @@
 package com.ciesielski.schoolapi.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.NoSuchElementException;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NoSuchSchoolException extends NoSuchElementException {
-
     public NoSuchSchoolException(final long id) {
         super("Could not find a school with ID: " + id);
     }
